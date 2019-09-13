ffe997fc3606654673a822df4e15e0594fc51d15
Andy Green <andy@warmcat.com>
2019-09-13 10:33:24 +0100

    h2: unify immortal stream tracking across SSE and ws substreams
    It was already correct but add helpers to isolate and deduplicate
    processing adding and closing a generically immortal stream.
    
    Change the default 31s h2 network connection timeout to be settable
    by .keepalive_timeout if nonzero.
    
    Add a public api allowing a client h2 stream to transition to
    half-closed LOCAL (by sending a 0-byte DATA with END_STREAM) and
    mark itself as immortal to create a read-only long-poll stream
    if the server allows it.
    
    Add a vhost server option flag LWS_SERVER_OPTION_VH_H2_HALF_CLOSED_LONG_POLL
    which allows the vhost to treat half-closed remotes as immortal long
    poll streams.

--- lib/core-net/close.c
+++ lib/core-net/close.c
@@ -229,6 +229,11 @@ __lws_close_free_wsi(struct lws *wsi, enum lws_close_status reason,
 	pt = &context->pt[(int)wsi->tsi];
 	lws_stats_bump(pt, LWSSTATS_C_API_CLOSE, 1);
 
+#if defined(LWS_WITH_HTTP2)
+	if (wsi->h2_stream_immortal)
+		lws_http_close_immortal(wsi);
+#endif
+
 	/* if we have children, close them first */
 	if (wsi->child_list) {
 		wsi2 = wsi->child_list;
